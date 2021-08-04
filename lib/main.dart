332703645e672952ce2560a594c4979fcce0d926
michael <maydev99@gmail.com>
2021-08-04 18:58:32 -0400

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MyApp());
 }
@@ -9,28 +10,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BasicApp',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-
-      home: MyHomePage(
-        title: 'BasicAppHomePage'
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(title: 'BasicAppHomePage'),
     );
   }
-
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   int _counter = 0;
 
   void _incrementCounter() {
@@ -45,52 +40,89 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _mainButton() {
+    print('Dialog Button Pressed');
+
+    // set up the button
+    Widget okButton = TextButton(
+      child: Text("cancel"),
+      onPressed: () {
+        Navigator.of(context).pop();
+        print('closed');
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("My title"),
+      content: Text("This is my message."),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Basic App'),
       ),
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:<Widget> [
+          children: <Widget>[
             Text(
               'Button Count:',
               style: TextStyle(fontSize: 20.0),
             ),
-            Text('$_counter',
-            style: TextStyle(fontSize: 50.0),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 50.0),
             ),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-
                 FloatingActionButton(
                   onPressed: _decrement,
                   tooltip: 'Decrement',
                   child: Icon(Icons.remove),
                 ),
-
                 FloatingActionButton(
                   onPressed: _incrementCounter,
                   tooltip: 'Increment',
                   child: Icon(Icons.add),
                 ),
-
-
               ],
             ),
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                MaterialButton(
+                  onPressed: _mainButton,
+                  child: Text('Dialog'),
+                  color: Colors.deepOrangeAccent,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  elevation: 7.0,
+                  clipBehavior: Clip.antiAlias,
+                ),
+              ]),
+            )
           ],
         ),
-
       ),
-
-
-
     );
   }
-
 }
